3208a14cc48126113bf557847e2fb429de4953df
LOLSU <reinhardt.alexander@gmail.com>
2020-03-25 20:10:53 +0100

    first and last commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const covid = require('novelcovid');
+const discord = require('discord.js')
+const id = '';
+const token = '';
+async function Corona() {
+   // Specific Country
+   let specificCountry = await covid.getCountry({country: 'Germany'});
+   return specificCountry;
+}
+Corona().then((country) => {
+    // console.log(country);
+
+    new discord.WebhookClient(id, token).send('Aktuelle Corona Anzahl Deutschland: ' + (country.cases - country.recovered) + '\n Infizierte pro 1ne million: '+ country.casesPerOneMillion + '\n Es haben sich aber schon ' + country.recovered + ' wieder erholt seit anfang der Pandemie. \n Heute wurden schon ' + country.todayCases + ' neue faelle verzeichnet. \n Es sind ' + country.deaths  + ' leute in Deutschland dran gestorben und alleine heute sind ' + country.todayDeaths + ' an covid-19 gestorben. \n Damit waere der Durchschnitt an toten pro eine million ' + country.deathsPerOneMillion).catch(console.error);
+})
