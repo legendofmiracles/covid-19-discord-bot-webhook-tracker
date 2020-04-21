53d7f3929aaf04ca6b4610aae1d02876312c634f
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-21 06:44:28 +0200

    lolsu

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const covid = require('novelcovid');
-const discord = require('discord.js')
-require('dotenv').config()
-const id = process.env.id;
-const token = process.env.token;
+const discord = require("discord.js");
+require("dotenv").config();
+const NovelCovid = require('novelcovid');
+
+
+
 async function Corona() {
-   // Specific Country
-   let specificCountry = await covid.getCountry({country: 'Germany'});
-   return specificCountry;
+  // Specific Country
+  const specificCountry = NovelCovid().countries('Germany');
+  return specificCountry;
 }
 Corona().then((country) => {
-    // console.log(country);
+  console.log(country);
+  const id = process.env.id;
+  const token = process.env.token;  
+  //new discord.WebhookClient(id, token).send('Aktuelle Corona Anzahl Deutschland: ' + (country.cases - country.recovered) + '\n Infizierte pro 1ne million: '+ country.casesPerOneMillion + '\n Es haben sich aber schon ' + country.recovered + ' wieder erholt seit anfang der Pandemie. \n Heute wurden schon ' + country.todayCases + ' neue faelle verzeichnet. \n Es sind ' + country.deaths  + ' leute in Deutschland dran gestorben und alleine heute sind ' + country.todayDeaths + ' an covid-19 gestorben. \n Damit waere der Durchschnitt an toten pro eine million ' + country.deathsPerOneMillion).catch(console.error);
+});
 
-    new discord.WebhookClient(id, token).send('Aktuelle Corona Anzahl Deutschland: ' + (country.cases - country.recovered) + '\n Infizierte pro 1ne million: '+ country.casesPerOneMillion + '\n Es haben sich aber schon ' + country.recovered + ' wieder erholt seit anfang der Pandemie. \n Heute wurden schon ' + country.todayCases + ' neue faelle verzeichnet. \n Es sind ' + country.deaths  + ' leute in Deutschland dran gestorben und alleine heute sind ' + country.todayDeaths + ' an covid-19 gestorben. \n Damit waere der Durchschnitt an toten pro eine million ' + country.deathsPerOneMillion).catch(console.error);
-})
